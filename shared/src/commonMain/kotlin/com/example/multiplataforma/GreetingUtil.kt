d0e6c566e775ced61041b5c30e6d5f157991087d
watereff <waterel12345@gmail.com>
2026-05-20 23:09:04 -0600

    agrego proyecto Android

---
+++ shared/src/commonMain/kotlin/com/example/multiplataforma/GreetingUtil.kt
@@ -0,0 +1,4 @@
+package com.example.multiplataforma
+
+fun sayHello(to: String): String =
+    "Hello, $to!"
---
+++ shared/src/iosMain/kotlin/com/example/multiplataforma/MainViewController.kt
@@ -0,0 +1,5 @@
+package com.example.multiplataforma
+
+import androidx.compose.ui.window.ComposeUIViewController
+
+fun MainViewController() = ComposeUIViewController { App() }
